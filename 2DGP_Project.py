a6ec52550079da2ac21fa7ba25bf0ba47a108818
dongPC@notebook <96215716+donggeon15@users.noreply.github.com>
2024-10-26 15:15:36 +0900

    클래스 틀 적용 배경 크기 수정

--- 2DGP_Project.py
+++ 2DGP_Project.py
@@ -3,20 +3,53 @@ from pico2d import *
 
 open_canvas()
 background = load_image('background1.png')
-character = load_image('walk_kobby.png')
+character = load_image('sword_kobby_sheet.png')
+
+
+class Kobby:
+    def __init__(self):
+        self.x,self.y = 0, 100
+        self.frame=0
+        self.dir=0
+        self.action=0
+        self.mode=0
+        self.image=load_image('nomal_kobby_sheet.png')
+
+    def update(self):
+        pass
+    def handle_event(self, event):
+        pass
+    def draw(self):
+        pass
+
+class Ground:
+    def __init__(self):
+        pass
+    def draw(self):
+        pass
+    def update(self):
+        pass
+
+class Monster:
+    def __init__(self):
+        pass
+    def update(self):
+        pass
+    def handle_event(self, event):
+        pass
+    def draw(self):
+        pass
 
 
 def handle_events():
     global running, dir, dir2
 
-    # fill here
     global x
     global left, up
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        # fill here
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
                 dir += 1
@@ -57,21 +90,20 @@ dir2 = 0
 left = False
 up = False
 
-# fill here
 while running:
     clear_canvas()
-    background.draw(400,300,800,600)
+    background.draw(750,300,1500,600)
     if left == True and up == False:
-        character.clip_draw(frame*25,0,25,25,x,y, 50, 50)
+        character.clip_draw(frame * 32, 80, 32, 40, x, y, 48, 60)
     elif left == False and up == False:
-        character.clip_draw(frame*25,0,25,25,x,y, 50, 50)
+        character.clip_draw(frame * 32, 80, 32, 40, x, y, 48, 60)
     elif left == False and up == True:
-        character.clip_draw(frame * 25, 0, 25, 25, x, y, 50, 50)
+        character.clip_draw(frame * 32, 80, 32, 40, x, y, 48, 60)
     elif left == True and up == True:
-        character.clip_draw(frame * 25, 0, 25, 25, x, y, 50, 50)
+        character.clip_draw(frame * 32, 80, 32, 40, x, y, 48, 60)
     update_canvas()
     handle_events()
-    frame = (frame + 1) % 10
+    frame = (frame + 1) % 11
     if dir > 0:
         if x < 800:
             x += dir * 5
